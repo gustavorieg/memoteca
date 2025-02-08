a716be05b4d43d87c92e736c39a104fe3253eeaa
Gustavo Rieg <gustavorieg10@gmail.com>
2025-02-08 13:27:28 -0300

    .

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -6,6 +6,7 @@ import { PensamentoService } from '../pensamento.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { randomFillSync, randomInt } from 'crypto';
+import * as moderndash from 'moderndash';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -24,9 +25,10 @@ import { randomFillSync, randomInt } from 'crypto';
 export class CriarPensamentoComponent{
 
   pensamento: Pensamento = {
+    id: moderndash.randomInt(1, 999),
     conteudo: '',
     autoria: '',
-    modelo: 'modelo1'
+    modelo: ''
   }
 
   constructor(
